82e54a385c9e888abddc0df72b867388e3502bc3
Durgaprasad-22 <ddprasad221@gmail.com>
2024-08-24 15:55:08 +0530

    adding another btn

--- Day3/GUI.py
+++ Day3/GUI.py
@@ -2,22 +2,34 @@ import tkinter  as tk
 
 root=tk.Tk()
 
-def get_btn():
+def get_btn1():
     input_data=textbox.get()
     label1.config(text=input_data)
+
+def get_btn2():
+    input_data=textbox.get()
+    label2.config(text=input_data)
+    
 
 root.title("hello")
 
 root.geometry("600x400")
 
 
-label1=tk.Label(root,text="WELCOME STANLEY")
+label1=tk.Label(root,text="first label")
 label1.pack(pady=10)
+
+label2=tk.Label(root,text="second label")
+label2.pack(pady=10)
 
 textbox=tk.Entry(root,width=50)
 textbox.pack(pady=20)
 
 
-btn=tk.Button(root,text="Button -1",command=get_btn)
-btn.pack(pady=10)
+btn1=tk.Button(root,text="Button -1",command=get_btn1)
+btn1.pack(pady=10)
+
+btn2=tk.Button(root,text="Button -2",command=get_btn2)
+btn2.pack(pady=10)
+
 
